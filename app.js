60c1a8975234ab7ccb05dffc35b027e0b0068720
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-09 19:49:45 -0800

    minor media queries and added event listener to nav links so the menu with close on mobile version when clicking links

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const menu = document.querySelector(".burger");
 const nav = document.querySelector(".nav-links");
+const link = document.querySelectorAll(".links");
 
 menu.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+link.forEach((l) =>
+  l.addEventListener("click", () => {
+    nav.classList.remove("active");
+  })
+);
